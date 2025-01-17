db597bf6cf8e64cd233db1a6bac55ae527ee0168
gerarddoblas <gerard.doblas@enti.cat>
2025-01-17 16:44:20 +0100

    Combat semi finished

--- AA3_Gerard-Doblas_Hector-Zornoza_SOL/AA3_Gerard-Doblas_Hector-Zornoza_PROJECT/Combat.cpp
+++ AA3_Gerard-Doblas_Hector-Zornoza_SOL/AA3_Gerard-Doblas_Hector-Zornoza_PROJECT/Combat.cpp
@@ -5,9 +5,26 @@
 void Combat::CombatChooise(Player& p, Enemy& e) {
 	std::cin >> chInput;
 	enemyAttack = (e.maxSTAM * 20)/100;
+	if (e.hp < ((e.maxHP * 30) / 100) && e.stam < ((e.maxSTAM * 30) / 100))
+	{
+		Defend = true;
+	}
+	else
+	{
+		Defend = false;
+	}
+	if (e.stam < ((e.maxSTAM * 20) / 100))
+	{
+		Rest = true;
+	}
+	else
+	{
+		Rest = false;
+	}
+
 	switch (chInput)
 	{
-	case 'A':
+	case 'A'://Si el player ataca
 		printf("\n");
 		printf("Enter the attack value (Max :%d): %", p.maxSTAM);
 		std::cin >> playerAttack;
@@ -47,21 +64,88 @@ void Combat::CombatChooise(Player& p, Enemy& e) {
 				printf("Enter a character to update the fight...");
 
 			}
-			else
+			else if(Rest)//Si el enemigo descansa
 			{
-
+				printf("");
+				e.hp = e.hp - playerAttack;
+				e.stam = e.maxSTAM;
+				printf("\n");
+				printf("Enter a character to update the fight...");
 			}
-			
-
-
 		}
 
 		break;
-	case 'D':
+	case 'D'://Si el player se defiende
+		printf("\n");
+		if (Defend == false && Rest == false)
+		{
+			printf("");
+			e.stam = e.stam - enemyAttack;
+			p.hp = p.hp - ((enemyAttack * 75) / 100);
+			p.stam = p.stam + ((p.maxSTAM * 25) / 100);
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
+		else if (Defend)//Si el enemigo se defiende
+		{
+			printf("");
+			e.stam = e.stam + ((e.maxSTAM * 25) / 100);
+			p.stam = p.stam + ((p.maxSTAM * 25) / 100);
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
+		else if (Rest)//Si el enemigo descansa
+		{
+			printf("");
+			e.stam = e.maxSTAM;
+			p.stam = p.stam + ((p.maxSTAM * 25) / 100);
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
 		break;
-	case 'R':
+	case 'R'://Si el player descansa
+		printf("\n");
+		p.stam = p.maxSTAM;
+		if (Defend == false && Rest == false)
+		{
+			printf("");
+			e.stam = e.stam - enemyAttack;
+			p.hp = p.hp - enemyAttack;
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
+		else if (Defend)
+		{
+			printf("");
+			e.stam = e.stam + ((e.maxSTAM * 25) / 100);
+			printf("\n");
+			printf("Enter a character to update the fight...");
+
+		}
+		else if (Rest)
+		{
+			printf("");
+			e.stam = e.maxSTAM;
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
 		break;
-	case 'P':
+	case 'P'://Si el player se toma una poción
+		printf("\n");
+		if (p.potions != 0)
+		{
+			printf("");
+			p.hp = ((p.hp * 40) / 100);
+			p.potions--;
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
+		else
+		{
+			printf("");
+			printf("\n");
+			printf("Enter a character to update the fight...");
+		}
 		break;
 	default:
 		break;
--- AA3_Gerard-Doblas_Hector-Zornoza_SOL/AA3_Gerard-Doblas_Hector-Zornoza_PROJECT/Enemy.h
+++ AA3_Gerard-Doblas_Hector-Zornoza_SOL/AA3_Gerard-Doblas_Hector-Zornoza_PROJECT/Enemy.h
@@ -5,6 +5,7 @@ struct Enemy
 	int hp = rand() % (MAX_INIT_HP_EN - MIN_INIT_HP_EN) + MIN_INIT_HP_EN;
 	int stam = rand() % (MAX_INIT_ST_EN - MIN_INIT_ST_EN) + MIN_INIT_ST_EN;
 	int maxSTAM = stam;
+	int maxHP = hp;
 	bool isDead;
 	char enemy = 'E';
 };
